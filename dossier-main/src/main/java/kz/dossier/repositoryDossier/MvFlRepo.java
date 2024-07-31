4c76e776cf49d60db66e7f01d1069bda5deea83b
Damir <damir.ps04@gmail.com>
2024-07-31 15:10:34 +0500

    mv fl and pension

--- dossier-main/src/main/java/kz/dossier/repositoryDossier/MvFlRepo.java
+++ dossier-main/src/main/java/kz/dossier/repositoryDossier/MvFlRepo.java
@@ -8,19 +8,19 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MvFlRepo extends JpaRepository<MvFl, Long> {
-    @Query(value= "select * from imp_kfm_fl.mv_fl mv_fl0_ where mv_fl0_.iin = ?1 ", nativeQuery = true)
+    @Query(value= "select * from imp_kfm_fl.mv_fl where iin = ?1 ", nativeQuery = true)
     List<MvFl> getUsersByLike(String iin);
     @Query(nativeQuery = true, value = "SELECT EXISTS(SELECT 1 FROM imp_kfm_fl.mv_fl WHERE iin = ?1 AND gender = 1)")
 
     Boolean getGender(String iin);
 
 
-    @Query(value= "select * from imp_kfm_fl.mv_fl mv_fl0_ where mv_fl0_.iin = ?1 limit 1", nativeQuery = true)
+    @Query(value= "select * from imp_kfm_fl.mv_fl where iin = ?1 limit 1", nativeQuery = true)
     MvFl getUserByIin(String iin);
 
-    @Query(value= "select * from imp_kfm_fl.mv_fl mv_fl0_ where mv_fl0_.iin = ?1 limit 1", nativeQuery = true)
+    @Query(value= "select * from imp_kfm_fl.mv_fl where iin = ?1 limit 1", nativeQuery = true)
     Optional<MvFl> getByIin(String iin);
 
-    @Query(value= "select * from imp_kfm_fl.mv_fl mv_fl0_ where first_name like ?1 and  patronymic like ?2 and last_name like ?3", nativeQuery = true)
+    @Query(value= "select * from imp_kfm_fl.mv_fl where first_name like ?1 and  patronymic like ?2 and last_name like ?3", nativeQuery = true)
     List<MvFl> getUsersByFIO(String name, String patronimic, String last);
 }
