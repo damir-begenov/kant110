c671c87953425172a031210ff000ccd725cd2332
Damir <damir.ps04@gmail.com>
2024-08-08 10:05:22 +0500

    sd

--- dossier-main/src/main/java/kz/dossier/repositoryDossier/MvFlRepo.java
+++ dossier-main/src/main/java/kz/dossier/repositoryDossier/MvFlRepo.java
@@ -13,7 +13,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface MvFlRepo extends JpaRepository<MvFl, Long> {
     @Query(
-            value = "SELECT   mv_fl0_.* , \n               nat.\"RU_NAME\" AS nationality_ru_name\n \n               FROM  imp_kfm_fl.mv_fl mv_fl0_\n            INNER JOIN \n                dictionary.d_nationality_new as nat ON nat.\"ID\"::text = mv_fl0_.nationality_id \n            WHERE \n                mv_fl0_.iin = ?1",
+            value = "SELECT   mv_fl0_.* , \n               nat.\"RU_NAME\" AS nationality_ru_name\n \n               FROM  imp_kfm_fl.mv_fl mv_fl0_\n            INNER JOIN \n                dictionary.d_nationality_new as nat ON nat.\"ID\"::text = mv_fl0_.nationality_id \n          WHERE \n                mv_fl0_.iin = ?1",
             nativeQuery = true
     )
     List<MvFl> getUsersByLike(String iin);
@@ -25,13 +25,13 @@ public interface MvFlRepo extends JpaRepository<MvFl, Long> {
     Boolean getGender(String iin);
 
     @Query(
-            value = "select * from imp_kfm_fl.mv_fl mv_fl0_ where mv_fl0_.iin = ?1 limit 1",
+            value = "SELECT   mv_fl0_.* , \n               nat.\"RU_NAME\" AS nationality_ru_name\n \n               FROM imp_kfm_fl.mv_fl mv_fl0_ INNER JOIN \n                dictionary.d_nationality_new as nat ON nat.\"ID\"::text = mv_fl0_.nationality_id \n          WHERE \n                mv_fl0_.iin = ?1 limit 1",
             nativeQuery = true
     )
     MvFl getUserByIin(String iin);
 
     @Query(
-            value = "select * from imp_kfm_fl.mv_fl mv_fl0_ where mv_fl0_.iin = ?1 limit 1",
+            value = "SELECT   mv_fl0_.* , \n               nat.\"RU_NAME\" AS nationality_ru_name\n \n               FROM imp_kfm_fl.mv_fl mv_fl0_  INNER JOIN \n                dictionary.d_nationality_new as nat ON nat.\"ID\"::text = mv_fl0_.nationality_id \n          WHERE \n                mv_fl0_.iin = ?1 limit 1",
             nativeQuery = true
     )
     Optional<MvFl> getByIin(String iin);
